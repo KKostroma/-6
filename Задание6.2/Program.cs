c343f356e5133af7382dd301ab4112a59434c2ba
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-11-23 00:11:31 +0300

    Завершено

---
+++ Задание6.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StarString = Console.ReadLine();
+            string Words = " ";
+            for (int i = StarString.Length - 1; i >= 0; i--)
+            {
+                Words += StarString[i];
+            }
+                if (StarString == Words)
+                    Console.WriteLine("Палиндром");
+                 else
+                    Console.WriteLine("Не палиндром");
+                Console.ReadKey();
+        }
+    }
+}
--- Задание6/Program.cs
+++ Задание6/Program.cs
@@ -10,6 +10,16 @@ namespace Задание6
     {
         static void Main(string[] args)
         {
+            string StarString = Console.ReadLine(); 
+            string[]Words = StarString.Split();
+            int max = 0;
+            foreach (string s in Words)
+            {
+                if (s.Length > max)
+                    max = s.Length;
+            }
+                Console.Write("Самое длинное слово: {0}", max);
+                Console.ReadKey();
         }
     }
 }
